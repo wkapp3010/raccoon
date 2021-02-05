f5ed7eb9bf68a87f9cc61ef35a15d24ba67007ca
heinzgrob <heinz.grob@firmtec.ch>
2021-02-05 14:39:28 +0100

    Update data.sql

--- src/main/resources/db/h2/data.sql
+++ src/main/resources/db/h2/data.sql
@@ -4,6 +4,8 @@ INSERT INTO vets VALUES (3, 'Linda', 'Douglas');
 INSERT INTO vets VALUES (4, 'Rafael', 'Ortega');
 INSERT INTO vets VALUES (5, 'Henry', 'Stevens');
 INSERT INTO vets VALUES (6, 'Sharon', 'Jenkins');
+INSERT INTO vets VALUES (7, 'Beat', 'Breu');
+INSERT INTO vets VALUES (8, 'Didier', 'Cuche');
 
 INSERT INTO specialties VALUES (1, 'radiology');
 INSERT INTO specialties VALUES (2, 'surgery');
